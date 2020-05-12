aa61e3c67cdd4d13b29835867929571bcb7d3873
Joseph Kalb <kalb.joseph@gmail.com>
2020-05-12 16:35:04 -0700

    first commit

---
+++ public/main.js
@@ -0,0 +1,10 @@
+const display = document.getElementById('display')
+
+let getSubPoints = async () => {
+    let result = await fetch(' https://decapi.me/twitch/subpoints/thethingssheplays')
+    let text = await result.text()
+    let trueCount = parseInt(text) - 1
+    display.innerText = trueCount
+}
+
+getSubPoints()
